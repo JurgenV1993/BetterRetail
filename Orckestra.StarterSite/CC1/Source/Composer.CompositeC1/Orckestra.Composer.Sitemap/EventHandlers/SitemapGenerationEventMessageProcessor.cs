9c9437bf63bb9773f1c19f040a62a7ad47353b4b
Alex <olek.honcharuk@orckestra.com>
2019-11-21 11:23:39 +0200

    add event handler

---
+++ Orckestra.StarterSite/CC1/Source/Composer.CompositeC1/Orckestra.Composer.Sitemap/EventHandlers/SitemapGenerationEventMessageProcessor.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Helpers;
+using Composite.Core;
+using Microsoft.ServiceBus.Messaging;
+using Orckestra.ExperienceManagement.Configuration.ServiceBus;
+
+namespace Orckestra.Composer.Sitemap.ServiceBus
+{
+    public class SitemapGenerationEventMessageProcessor : IMessageProcessor
+    {
+        public string EventName { set; get; }
+
+        public async Task ProcessMessageAsync(BrokeredMessage message, CancellationToken cancellationToken)
+        {
+            if (!message.ContentType.Contains(EventName)) return;
+
+            
+        }
+    }
+}
--- Orckestra.StarterSite/CC1/Source/Composer.CompositeC1/Orckestra.Composer.Sitemap/StartupHandler.cs
+++ Orckestra.StarterSite/CC1/Source/Composer.CompositeC1/Orckestra.Composer.Sitemap/StartupHandler.cs
@@ -7,6 +7,10 @@ using Composite.Core.Application;
 using Microsoft.Extensions.DependencyInjection;
 using Orckestra.Composer.CompositeC1.Sitemap;
 using Orckestra.Composer.SearchQuery.Repositories;
+using Orckestra.Composer.Sitemap.ServiceBus;
+using Orckestra.ExperienceManagement.Configuration.ServiceBus;
+using Orckestra.ExperienceManagement.Configuration.Settings;
+using Composite.Core;
 
 namespace Orckestra.Composer.Sitemap
 {
@@ -20,7 +24,10 @@ namespace Orckestra.Composer.Sitemap
 
         public static void OnInitialized()
         {
-
+            var settings = ServiceLocator.GetService<IServiceBusSettings>();
+            var listener = new ServiceBusListener(settings, "Events");
+            listener.Register(new SitemapGenerationEventMessageProcessor() { EventName = "ProductUpdatedEvent" });
+            listener.Start();
         }
 
         public static void ConfigureServices(IServiceCollection collection)
