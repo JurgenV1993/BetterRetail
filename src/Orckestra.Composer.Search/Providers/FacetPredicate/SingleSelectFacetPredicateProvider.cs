4f3f26f1b7f87eb96dca0335e7ec72c780a97993
vadym.hyryn <vadym.hyryn@orckestra.com>
2020-04-17 16:40:23 +0300

    Update SingleSelectFacetPredicateProvider.cs

--- src/Orckestra.Composer.Search/Providers/FacetPredicate/SingleSelectFacetPredicateProvider.cs
+++ src/Orckestra.Composer.Search/Providers/FacetPredicate/SingleSelectFacetPredicateProvider.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using Orckestra.Composer.Utils;
 using Orckestra.Overture.ServiceModel.Search;
+using static Orckestra.Composer.Utils.MessagesHelper.ArgumentException;
 using SearchFilter = Orckestra.Composer.Parameters.SearchFilter;
 
 namespace Orckestra.Composer.Search.Providers.FacetPredicate
@@ -14,15 +14,8 @@ namespace Orckestra.Composer.Search.Providers.FacetPredicate
         /// <param name="filter">Filter to create the facet predicate from.</param>
         public Overture.ServiceModel.Search.FacetPredicate CreateFacetPredicate(SearchFilter filter)
         {
-            if (filter == null)
-            {
-                throw new ArgumentNullException("filter");
-            }
-
-            if (string.IsNullOrWhiteSpace(filter.Name))
-            {
-                throw new ArgumentException(ArgumentNullMessageFormatter.FormatErrorMessage("Name"), "filter");
-            }
+            if (filter == null) { throw new ArgumentNullException(nameof(filter)); }
+            if (string.IsNullOrWhiteSpace(filter.Name)) { throw new ArgumentException(GetMessageOfNullWhiteSpace(nameof(filter.Name)), nameof(filter));}
 
             if (string.IsNullOrWhiteSpace(filter.Value))
             {
