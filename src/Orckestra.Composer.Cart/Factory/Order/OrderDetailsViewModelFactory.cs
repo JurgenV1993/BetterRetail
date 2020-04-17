3a54545e6b60c091e501f403b6f6b59ca96828f3
vadym.hyryn <vadym.hyryn@orckestra.com>
2020-04-17 11:23:12 +0300

    Fix OrderDetailsViewModelFactory.cs
    - Avoid exceptions hardcoded dependency on parameter names and names of properties
    - Simplify null checks
    - Fix GetOrderChangesViewModel method. Using HashSet instead of transformation an array to list + LINQ + FindIndex combination since it is slow
    - Fix GetBillingAddressViewModel method. Avoid redundant calls, refactoring
    - Fix MapOrderChangeViewModel method. Refactoring
    - Fix GetShippingMethodViewModel method. Refactoring
    - Fix GetPaymentViewModels method. Refactoring
    - Fix GetActiveShipments method. Refactoring

--- src/Orckestra.Composer.Cart/Factory/Order/OrderDetailsViewModelFactory.cs
+++ src/Orckestra.Composer.Cart/Factory/Order/OrderDetailsViewModelFactory.cs
@@ -12,6 +12,7 @@ using Orckestra.Composer.Providers;
 using Orckestra.Composer.Providers.Localization;
 using Orckestra.Composer.ViewModels;
 using Orckestra.Overture.ServiceModel.Orders;
+using static Orckestra.Composer.Utils.MessagesHelper.ArgumentException;
 
 namespace Orckestra.Composer.Cart.Factory.Order
 {
@@ -44,27 +45,16 @@ namespace Orckestra.Composer.Cart.Factory.Order
             ILineItemViewModelFactory lineItemViewModelFactory,
             IRewardViewModelFactory rewardViewModelFactory)
         {
-            if (localizationProvider == null) { throw new ArgumentNullException("localizationProvider"); }
-            if (viewModelMapper == null) { throw new ArgumentNullException("viewModelMapper"); }
-            if (productUrlProvider == null) { throw new ArgumentNullException("productUrlProvider"); }
-            if (cartViewModelFactory == null) { throw new ArgumentNullException("cartViewModelFactory"); }
-            if (countryService == null) { throw new ArgumentNullException("countryService"); }
-            if (paymentProviderFactory == null) { throw new ArgumentNullException("paymentProviderFactory"); }
-            if (shippingTrackingProviderFactory == null) { throw new ArgumentNullException("shippingTrackingProviderFactory"); }
-            if (taxViewModelFactory == null) { throw new ArgumentNullException("taxViewModelFactory"); }
-            if (lineItemViewModelFactory == null) { throw new ArgumentNullException("lineItemViewModelFactory"); }
-            if (rewardViewModelFactory == null) { throw new ArgumentNullException("rewardViewModelFactory"); }
-
-            LocalizationProvider = localizationProvider;
-            ViewModelMapper = viewModelMapper;
-            CountryService = countryService;
-            ProductUrlProvider = productUrlProvider;
-            CartViewModelFactory = cartViewModelFactory;
-            PaymentProviderFactory = paymentProviderFactory;
-            ShippingTrackingProviderFactory = shippingTrackingProviderFactory;
-            TaxViewModelFactory = taxViewModelFactory;
-            LineItemViewModelFactory = lineItemViewModelFactory;
-            RewardViewModelFactory = rewardViewModelFactory;
+            LocalizationProvider = localizationProvider ?? throw new ArgumentNullException(nameof(localizationProvider));
+            ViewModelMapper = viewModelMapper ?? throw new ArgumentNullException(nameof(viewModelMapper));
+            CountryService = countryService ?? throw new ArgumentNullException(nameof(countryService));
+            ProductUrlProvider = productUrlProvider ?? throw new ArgumentNullException(nameof(productUrlProvider));
+            CartViewModelFactory = cartViewModelFactory ?? throw new ArgumentNullException(nameof(cartViewModelFactory));
+            PaymentProviderFactory = paymentProviderFactory ?? throw new ArgumentNullException(nameof(paymentProviderFactory));
+            ShippingTrackingProviderFactory = shippingTrackingProviderFactory ?? throw new ArgumentNullException(nameof(shippingTrackingProviderFactory));
+            TaxViewModelFactory = taxViewModelFactory ?? throw new ArgumentNullException(nameof(taxViewModelFactory));
+            LineItemViewModelFactory = lineItemViewModelFactory ?? throw new ArgumentNullException(nameof(lineItemViewModelFactory));
+            RewardViewModelFactory = rewardViewModelFactory ?? throw new ArgumentNullException(nameof(rewardViewModelFactory));
         }
 
         /// <summary>
@@ -74,21 +64,21 @@ namespace Orckestra.Composer.Cart.Factory.Order
         /// <returns></returns>
         public virtual OrderDetailViewModel CreateViewModel(CreateOrderDetailViewModelParam param)
         {
-            if (param == null) { throw new ArgumentNullException("param"); }
-            if (param.CultureInfo == null) { throw new ArgumentException("param.CultureInfo"); }
-            if (param.Order == null) { throw new ArgumentException("param.Order"); }
-            if (param.OrderStatuses == null) { throw new ArgumentException("param.OrderStatuses"); }
-            if (param.ShipmentStatuses == null) { throw new ArgumentException("param.ShipmentStatuses"); }
-            if (param.OrderChanges == null) { throw new ArgumentException("param.OrderChanges"); }
-            if (param.ProductImageInfo == null) { throw new ArgumentException("param.ProductImageInfo"); }
-            if (param.ProductImageInfo.ImageUrls == null) { throw new ArgumentException("param.ImageUrls"); }
-            if (string.IsNullOrWhiteSpace(param.BaseUrl)) { throw new ArgumentException("param.BaseUrl"); }
-            if (string.IsNullOrWhiteSpace(param.CountryCode)) { throw new ArgumentException("param.CountryCode"); }
+            if (param == null) { throw new ArgumentNullException(nameof(param)); }
+            if (param.CultureInfo == null) { throw new ArgumentException(GetMessageOfNull(nameof(param.CultureInfo)), nameof(param)); }
+            if (param.Order == null) { throw new ArgumentException(GetMessageOfNull(nameof(param.Order)), nameof(param)); }
+            if (param.OrderStatuses == null) { throw new ArgumentException(GetMessageOfNull(nameof(param.OrderStatuses)), nameof(param)); }
+            if (param.ShipmentStatuses == null) { throw new ArgumentException(GetMessageOfNull(nameof(param.ShipmentStatuses)), nameof(param)); }
+            if (param.OrderChanges == null) { throw new ArgumentException(GetMessageOfNull(nameof(param.OrderChanges)), nameof(param)); }
+            if (param.ProductImageInfo == null) { throw new ArgumentException(GetMessageOfNull(nameof(param.ProductImageInfo)), nameof(param)); }
+            if (param.ProductImageInfo.ImageUrls == null) { throw new ArgumentException(GetMessageOfNull(nameof(param.ProductImageInfo.ImageUrls)), nameof(param)); }
+            if (string.IsNullOrWhiteSpace(param.BaseUrl)) { throw new ArgumentException(GetMessageOfNullWhiteSpace(nameof(param.BaseUrl)), nameof(param)); }
+            if (string.IsNullOrWhiteSpace(param.CountryCode)) { throw new ArgumentException(GetMessageOfNullWhiteSpace(nameof(param.CountryCode)), nameof(param)); }
 
             var viewModel = new OrderDetailViewModel();
             var shipments = GetActiveShipments(param.Order);
 
-#pragma warning disable 618
+            #pragma warning disable 618
             viewModel.OrderInfos = GetOrderInfosViewModel(param);
             viewModel.History = GetOrderChangesViewModel(param.OrderChanges, param.CultureInfo, _orderStatus);
             viewModel.BillingAddress = GetBillingAddressViewModel(param);
@@ -99,7 +89,7 @@ namespace Orckestra.Composer.Cart.Factory.Order
             viewModel.OrderSummary = CartViewModelFactory.GetOrderSummaryViewModel(param.Order.Cart, param.CultureInfo);
             viewModel.OrderSummary.Taxes = TaxViewModelFactory.CreateTaxViewModels(shipments.SelectMany(s=>s.Taxes).ToList(), param.CultureInfo).ToList();
             MapAdditionalFees(viewModel, param);
-#pragma warning restore 618
+            #pragma warning restore 618
 
             // Reverse the items order in the Cart so the last added item will be the first in the list
             if (viewModel.Shipments != null && viewModel.Shipments.Any())
@@ -112,7 +102,7 @@ namespace Orckestra.Composer.Cart.Factory.Order
 
         protected virtual OrderDetailInfoViewModel GetOrderInfosViewModel(CreateOrderDetailViewModelParam param)
         {
-            if (param.Order.Cart.Total == null) { throw new ArgumentNullException("param.Order.Cart.Total"); }
+            if (param.Order.Cart.Total == null) { throw new ArgumentException(GetMessageOfNull(nameof(param.Order.Cart.Total)), nameof(param)); }
 
             var orderInfos = ViewModelMapper.MapTo<OrderDetailInfoViewModel>(param.Order, param.CultureInfo);
 
@@ -127,16 +117,20 @@ namespace Orckestra.Composer.Cart.Factory.Order
         protected virtual List<OrderChangeViewModel> GetOrderChangesViewModel(IEnumerable<OrderHistoryItem> orderChanges, CultureInfo cultureInfo, params string[] historyCategories)
         {
             var history = new List<OrderChangeViewModel>();
-            var historyCategoriesList = historyCategories.ToList();
-
-            var changes = orderChanges.Where(h => historyCategoriesList.FindIndex(x => x.Equals(h.Category, StringComparison.InvariantCultureIgnoreCase)) >= 0);
-
-            foreach (var change in changes)
+            var hashSet = new HashSet<string>(StringComparer.InvariantCultureIgnoreCase);
+            foreach (var el in historyCategories)
             {
-                var changeVm = MapOrderChangeViewModel(change, cultureInfo);
-                history.Add(changeVm);
+                hashSet.Add(el);
             }
 
+            foreach (var change in orderChanges)
+            {
+                if (hashSet.Contains(change.Category))
+                {
+                    var changeVm = MapOrderChangeViewModel(change, cultureInfo);
+                    history.Add(changeVm);
+                }
+            }
             return history;
         }
 
@@ -157,15 +151,9 @@ namespace Orckestra.Composer.Cart.Factory.Order
         [Obsolete("This function does not support multiple shipments. Use GetAddressViewModel instead")]
         protected virtual AddressViewModel GetBillingAddressViewModel(CreateOrderDetailViewModelParam param)
         {
-            var validPayments = param.Order.Cart.Payments.Where(x => !x.IsVoided()).ToList();
-            var payment = validPayments.FirstOrDefault() ?? param.Order.Cart.Payments.FirstOrDefault();
-
-            if (payment == null)
-            {
-                return null;
-            }
-
-            return CartViewModelFactory.GetAddressViewModel(payment.BillingAddress, param.CultureInfo);
+            var validPayment = param.Order.Cart.Payments.Where(x => !x.IsVoided()).FirstOrDefault();
+            var payment = validPayment ?? param.Order.Cart.Payments.FirstOrDefault();
+            return payment == null ? null : CartViewModelFactory.GetAddressViewModel(payment.BillingAddress, param.CultureInfo);
         }
 
         protected virtual List<OrderShipmentDetailViewModel> GetShipmentViewModels(CreateOrderDetailViewModelParam param)
@@ -183,9 +171,7 @@ namespace Orckestra.Composer.Cart.Factory.Order
 
         protected virtual OrderChangeViewModel MapOrderChangeViewModel(OrderHistoryItem orderHistoryItem, CultureInfo cultureInfo)
         {
-           var orderChangeVm = ViewModelMapper.MapTo<OrderChangeViewModel>(orderHistoryItem, cultureInfo);
-
-            return orderChangeVm;
+            return ViewModelMapper.MapTo<OrderChangeViewModel>(orderHistoryItem, cultureInfo);
         }
 
         protected virtual OrderShipmentDetailViewModel GetOrderShipmentDetailViewModel(Shipment shipment, CreateOrderDetailViewModelParam param)
@@ -253,12 +239,9 @@ namespace Orckestra.Composer.Cart.Factory.Order
         [Obsolete("This function does not support multiple shipments. Use the overloaded GetShippingMethodViewModel with shipment instead")]
         protected virtual OrderShippingMethodViewModel GetShippingMethodViewModel(CreateOrderDetailViewModelParam param)
         {
-            if (param.Order.Cart.Shipments == null) { throw new ArgumentNullException("param.Order.Cart.Shipments"); }
+            if (param.Order.Cart.Shipments == null) { throw new ArgumentException(GetMessageOfNull(nameof(param.Order.Cart.Shipments)), nameof(param)); }
 
-            var shipments = GetActiveShipments(param.Order);
-
-            var shipment = shipments.FirstOrDefault();
-
+            var shipment = GetActiveShipments(param.Order).FirstOrDefault();
             var shippingMethodVm = GetShippingMethodViewModel(shipment, param);
 
             return shippingMethodVm;
@@ -266,7 +249,7 @@ namespace Orckestra.Composer.Cart.Factory.Order
 
         protected virtual OrderShippingMethodViewModel GetShippingMethodViewModel(Shipment shipment, CreateOrderDetailViewModelParam param)
         {
-            if (param.Order.Cart.Shipments == null) { throw new ArgumentNullException("param.Order.Cart.Shipments"); }
+            if (param.Order.Cart.Shipments == null) { throw new ArgumentException(GetMessageOfNull(nameof(param.Order.Cart.Shipments)), nameof(param)); }
 
             if (shipment == null)
             {
@@ -295,7 +278,7 @@ namespace Orckestra.Composer.Cart.Factory.Order
         {
             var paymentVMs = new List<OrderSummaryPaymentViewModel>();
             var validPayments = param.Order.Cart.Payments.Where(x => !x.IsVoided()).ToList();
-            var payments = validPayments.Any() ? validPayments : param.Order.Cart.Payments.ToList();
+            var payments = validPayments.Any() ? validPayments : param.Order.Cart.Payments;
 
             foreach (var payment in payments)
             {
@@ -326,14 +309,7 @@ namespace Orckestra.Composer.Cart.Factory.Order
 
         protected virtual IEnumerable<Shipment> GetActiveShipments(Overture.ServiceModel.Orders.Order order)
         {
-            if (order.OrderStatus.Equals(_orderStatusCanceled))
-            {
-                return order.Cart.Shipments;
-            }
-            else
-            {
-                return order.Cart.GetActiveShipments();
-            }
+            return order.OrderStatus.Equals(_orderStatusCanceled) ? order.Cart.Shipments : order.Cart.GetActiveShipments();
         }
     }
 }
