45d30e642fc1d039a1e2d28fafbd699468067ec3
vadym.hyryn <vadym.hyryn@orckestra.com>
2020-04-21 11:58:26 +0300

    Update RecurringOrderTemplateController.cs

--- src/Orckestra.Composer/Api/RecurringOrderTemplateController.cs
+++ src/Orckestra.Composer/Api/RecurringOrderTemplateController.cs
@@ -1,15 +1,12 @@
-﻿using Orckestra.Composer.Parameters;
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Orckestra.Composer.Parameters;
 using Orckestra.Composer.Request;
 using Orckestra.Composer.Requests;
 using Orckestra.Composer.Services;
 using Orckestra.Composer.Utils;
 using Orckestra.Composer.WebAPIFilters;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web.Http;
 
 namespace Orckestra.Composer.Api
 {
@@ -24,18 +21,18 @@ namespace Orckestra.Composer.Api
             IRecurringOrderTemplatesViewService recurringOrderTemplatesViewService,
             IComposerContext composerContext)
         {
-            if (recurringOrderTemplatesViewService == null) { throw new ArgumentNullException(nameof(recurringOrderTemplatesViewService)); }
-            if (composerContext == null) { throw new ArgumentNullException(nameof(composerContext)); }
+            RecurringOrderTemplatesViewService = recurringOrderTemplatesViewService 
+                ?? throw new ArgumentNullException(nameof(recurringOrderTemplatesViewService));
 
-            RecurringOrderTemplatesViewService = recurringOrderTemplatesViewService;
-            ComposerContext = composerContext;
+            ComposerContext = composerContext ?? throw new ArgumentNullException(nameof(composerContext));
         }
 
         [HttpGet]
         [ActionName("getrecurringordertemplates")]
         public virtual async Task<IHttpActionResult> GetRecurringOrderTemplates()
         {
-            var vm = await RecurringOrderTemplatesViewService.GetRecurringOrderTemplatesViewModelAsync(new GetRecurringOrderTemplatesParam {
+            var vm = await RecurringOrderTemplatesViewService.GetRecurringOrderTemplatesViewModelAsync(new GetRecurringOrderTemplatesParam 
+            {
                 Scope = ComposerContext.Scope,
                 CustomerId = ComposerContext.CustomerId,
                 CultureInfo = ComposerContext.CultureInfo,
@@ -75,7 +72,7 @@ namespace Orckestra.Composer.Api
             if (string.IsNullOrWhiteSpace(request.LineItemId)) { return BadRequest("Invalid lineItemId"); }
             if (!ModelState.IsValid) { return BadRequest(ModelState); }
 
-            var results = await RecurringOrderTemplatesViewService.RemoveRecurringOrderTemplateLineItemAsync(new RemoveRecurringOrderTemplateLineItemParam()
+            var results = await RecurringOrderTemplatesViewService.RemoveRecurringOrderTemplateLineItemAsync(new RemoveRecurringOrderTemplateLineItemParam
             {
                 Culture = ComposerContext.CultureInfo,
                 ScopeId = ComposerContext.Scope,
@@ -94,7 +91,8 @@ namespace Orckestra.Composer.Api
             if (request.LineItemsIds.Count == 0) { return BadRequest("Invalid lineItemsIds"); }
             if (!ModelState.IsValid) { return BadRequest(ModelState); }
 
-            var results = await RecurringOrderTemplatesViewService.RemoveRecurringOrderTemplatesLineItemsAsync(new RemoveRecurringOrderTemplateLineItemsParam()
+            var results = await RecurringOrderTemplatesViewService
+                .RemoveRecurringOrderTemplatesLineItemsAsync(new RemoveRecurringOrderTemplateLineItemsParam
             {
                 Culture = ComposerContext.CultureInfo,
                 ScopeId = ComposerContext.Scope,
@@ -113,12 +111,12 @@ namespace Orckestra.Composer.Api
             if (request == null) { return BadRequest("Missing Request Body"); }
             if (!ModelState.IsValid) { return BadRequest(ModelState); }
             
-            var results = await RecurringOrderTemplatesViewService.UpdateRecurringOrderTemplateLineItemAsync(new UpdateRecurringOrderTemplateLineItemParam {
+            var results = await RecurringOrderTemplatesViewService.UpdateRecurringOrderTemplateLineItemAsync(new UpdateRecurringOrderTemplateLineItemParam 
+            {
                 ScopeId = ComposerContext.Scope,
                 CustomerId = ComposerContext.CustomerId,
                 CultureInfo = ComposerContext.CultureInfo,
                 BaseUrl = RequestUtils.GetBaseUrl(Request).ToString(),
-
                 BillingAddressId = request.BillingAddressId,
                 LineItemId = request.LineItemId,
                 NextOccurence = request.NextOccurence,
@@ -140,8 +138,7 @@ namespace Orckestra.Composer.Api
             if (string.IsNullOrEmpty(request.RecurringOrderTemplateId)) { return BadRequest("Missing Request Body"); }
             if (!ModelState.IsValid) { return BadRequest(ModelState); }
 
-            Guid guid;
-            Guid.TryParse(request.RecurringOrderTemplateId, out guid);
+            Guid.TryParse(request.RecurringOrderTemplateId, out Guid guid);
 
             var vm = await RecurringOrderTemplatesViewService.GetRecurringOrderTemplateDetailViewModelAsync(new GetRecurringOrderTemplateDetailParam
             {
