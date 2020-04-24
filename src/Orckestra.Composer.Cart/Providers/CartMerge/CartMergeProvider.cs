8f85f3520cc382240ea5cb1329594cda922c53f1
vadym.hyryn <vadym.hyryn@orckestra.com>
2020-04-24 15:43:01 +0300

    Update CartMergeProvider.cs

--- src/Orckestra.Composer.Cart/Providers/CartMerge/CartMergeProvider.cs
+++ src/Orckestra.Composer.Cart/Providers/CartMerge/CartMergeProvider.cs
@@ -15,135 +15,122 @@ using static Orckestra.Composer.Utils.MessagesHelper.ArgumentException;
 
 namespace Orckestra.Composer.Cart.Providers.CartMerge
 {
-    public class CartMergeProvider : ICartMergeProvider
-    {
-        protected virtual ICartRepository CartRepository { get; private set; }
-        protected virtual IFixCartService FixCartService { get; private set; }
+	public class CartMergeProvider : ICartMergeProvider
+	{
+		protected virtual ICartRepository CartRepository { get; private set; }
+		protected virtual IFixCartService FixCartService { get; private set; }
 
-        public CartMergeProvider(ICartRepository cartRepository, IFixCartService fixCartService)
-        {
-            CartRepository = cartRepository ?? throw new ArgumentNullException(nameof(cartRepository));
-            FixCartService = fixCartService ?? throw new ArgumentNullException(nameof(fixCartService));
-        }
+		public CartMergeProvider(ICartRepository cartRepository, IFixCartService fixCartService)
+		{
+			CartRepository = cartRepository ?? throw new ArgumentNullException(nameof(cartRepository));
+			FixCartService = fixCartService ?? throw new ArgumentNullException(nameof(fixCartService));
+		}
 
-        /// <summary>
-        /// Merges the lineitems of a guest customer's cart and a logged customer's cart.
-        /// </summary>
-        /// <param name="param"></param>
-        /// <returns></returns>
-        public virtual async Task MergeCartAsync(CartMergeParam param)
-        {
-            if (param == null) { throw new ArgumentNullException(nameof(param)); }
-            if (param.GuestCustomerId == Guid.Empty) { throw new ArgumentException(GetMessageOfEmpty(nameof(param.GuestCustomerId)), nameof(param)); }
-            if (param.LoggedCustomerId == Guid.Empty) { throw new ArgumentException(GetMessageOfEmpty(nameof(param.LoggedCustomerId)), nameof(param)); }
-            if (string.IsNullOrEmpty(param.Scope)) { throw new ArgumentException(GetMessageOfNullEmpty(nameof(param.Scope)), nameof(param)); }
-            if (param.GuestCustomerId == param.LoggedCustomerId) { return; }
+		/// <summary>
+		/// Merges the lineitems of a guest customer's cart and a logged customer's cart.
+		/// </summary>
+		/// <param name="param"></param>
+		/// <returns></returns>
+		public virtual async Task MergeCartAsync(CartMergeParam param)
+		{
+			if (param == null) { throw new ArgumentNullException(nameof(param)); }
+			if (param.GuestCustomerId == Guid.Empty) { throw new ArgumentException(GetMessageOfEmpty(nameof(param.GuestCustomerId)), nameof(param)); }
+			if (param.LoggedCustomerId == Guid.Empty) { throw new ArgumentException(GetMessageOfEmpty(nameof(param.LoggedCustomerId)), nameof(param)); }
+			if (string.IsNullOrEmpty(param.Scope)) { throw new ArgumentException(GetMessageOfNullEmpty(nameof(param.Scope)), nameof(param)); }
+			if (param.GuestCustomerId == param.LoggedCustomerId) { return; }
 
-            var getLoggedCustomerCartTask = CartRepository.GetCartAsync(new GetCartParam
-            {
-                CustomerId = param.LoggedCustomerId,
-                CartName = CartConfiguration.ShoppingCartName,
-                Scope = param.Scope,
-                CultureInfo = CultureInfo.InvariantCulture
-            });
+			var getLoggedCustomerCartTask = CartRepository.GetCartAsync(new GetCartParam
+			{
+				CustomerId = param.LoggedCustomerId,
+				CartName = CartConfiguration.ShoppingCartName,
+				Scope = param.Scope,
+				CultureInfo = CultureInfo.InvariantCulture
+			});
 
-            var getGuestCustomerCartTask = CartRepository.GetCartAsync(new GetCartParam
-            {
-                CustomerId = param.GuestCustomerId,
-                CartName = CartConfiguration.ShoppingCartName,
-                Scope = param.Scope,
-                CultureInfo = CultureInfo.InvariantCulture
-            });
+			var getGuestCustomerCartTask = CartRepository.GetCartAsync(new GetCartParam
+			{
+				CustomerId = param.GuestCustomerId,
+				CartName = CartConfiguration.ShoppingCartName,
+				Scope = param.Scope,
+				CultureInfo = CultureInfo.InvariantCulture
+			});
 
-            var result = await Task.WhenAll(getLoggedCustomerCartTask, getGuestCustomerCartTask).ConfigureAwait(false);
+			var result = await Task.WhenAll(getLoggedCustomerCartTask, getGuestCustomerCartTask).ConfigureAwait(false);
 
-            var loggedCustomerCart = result[0];
-            var guestCustomerCart = result[1];
+			var loggedCustomerCart = result[0];
+			var guestCustomerCart = result[1];
 
-            var guestCustomerLineItems = guestCustomerCart.GetLineItems();
+			var guestCustomerLineItems = guestCustomerCart.GetLineItems();
 
-            if (!guestCustomerLineItems.Any())
-            {
-                return;
-            }
+			if (!guestCustomerLineItems.Any())
+			{
+				return;
+			}
 
-            var loggedCustomerLineItems = loggedCustomerCart.GetLineItems();
-            loggedCustomerCart.Shipments.First().LineItems = MergeLineItems(guestCustomerLineItems, loggedCustomerLineItems);
-            loggedCustomerCart.Coupons = MergeCoupons(guestCustomerCart.Coupons, loggedCustomerCart.Coupons);
+			var loggedCustomerLineItems = loggedCustomerCart.GetLineItems();
 
-            var cart = await CartRepository.UpdateCartAsync(UpdateCartParamFactory.Build(loggedCustomerCart)).ConfigureAwait(false);
+			loggedCustomerCart.Shipments.First().LineItems = MergeLineItems(guestCustomerLineItems, loggedCustomerLineItems);
+			loggedCustomerCart.Coupons = MergeCoupons(guestCustomerCart.Coupons, loggedCustomerCart.Coupons);
 
-            await FixCartService.FixCartAsync(new FixCartParam
-            {
-                Cart = cart
-            });
-        }
+			var cart = await CartRepository.UpdateCartAsync(UpdateCartParamFactory.Build(loggedCustomerCart)).ConfigureAwait(false);
 
-        protected virtual List<Coupon> MergeCoupons(List<Coupon> guestCoupons, List<Coupon> loggedCustomerCoupons)
-        {
-            var dictionary = new Dictionary<string, Coupon>();
+			await FixCartService.FixCartAsync(new FixCartParam
+			{
+				Cart = cart
+			});
+		}
 
-            if (loggedCustomerCoupons != null)
-            {
-                foreach (var loggedCustomerCoupon in loggedCustomerCoupons)
-                {
-                    if (dictionary.ContainsKey(loggedCustomerCoupon.CouponCode))
-                    {
-                        continue;
-                    }
-                    dictionary.Add(loggedCustomerCoupon.CouponCode, loggedCustomerCoupon);
-                }
-            }
+		protected virtual List<Coupon> MergeCoupons(List<Coupon> guestCoupons, List<Coupon> loggedCustomerCoupons)
+		{
+			if (guestCoupons == null || !guestCoupons.Any()) { return loggedCustomerCoupons; }
 
-            if (guestCoupons != null)
-            {
-                foreach (var guestCoupon in guestCoupons)
-                {
-                    if (dictionary.ContainsKey(guestCoupon.CouponCode))
-                    {
-                        continue;
-                    }
-                    dictionary.Add(guestCoupon.CouponCode, guestCoupon);
-                }
-            }
-            return dictionary.Values.ToList();
-        }
+			var hashSet = new HashSet<string>(loggedCustomerCoupons.Select(x => x.CouponCode));
 
-        protected virtual List<LineItem> MergeLineItems(List<LineItem> guestLineItems, List<LineItem> loggedLineItems)
-        {
-            var dictionary = new Dictionary<(string, string), LineItem>();
+			foreach (var guestCoupon in guestCoupons)
+			{
+				if (hashSet.Contains(guestCoupon.CouponCode)) { continue; }
 
-            if (guestLineItems != null)
-            {
-                foreach (var guestLineItem in guestLineItems)
-                {
-                    if (dictionary.ContainsKey((guestLineItem.ProductId, guestLineItem.VariantId)))
-                    {
-                        dictionary[(guestLineItem.ProductId, guestLineItem.VariantId)].Quantity += guestLineItem.Quantity;
-                    }
-                    else
-                    {
-                        dictionary.Add((guestLineItem.ProductId, guestLineItem.VariantId), guestLineItem);
-                    }
-                }
-            }
+				hashSet.Add(guestCoupon.CouponCode);
+				loggedCustomerCoupons.Add(guestCoupon);
+			}
+			return loggedCustomerCoupons;
+		}
 
-            if (loggedLineItems != null)
-            {
-                foreach (var loggedLineItem in loggedLineItems)
-                {
-                    if (dictionary.ContainsKey((loggedLineItem.ProductId, loggedLineItem.VariantId)))
-                    {
-                        dictionary[(loggedLineItem.ProductId, loggedLineItem.VariantId)].Quantity += loggedLineItem.Quantity;
-                    }
-                    else
-                    {
-                        dictionary.Add((loggedLineItem.ProductId, loggedLineItem.VariantId), loggedLineItem);
-                    }
-                }
-            }
+		protected virtual List<LineItem> MergeLineItems(List<LineItem> guestLineItems, List<LineItem> loggedLineItems)
+		{
+			var dictionary = new Dictionary<(string, string), LineItem>();
 
-            return dictionary.Values.ToList();
-        }
-    }
+			if (guestLineItems != null)
+			{
+				foreach (var guestLineItem in guestLineItems)
+				{
+					if (dictionary.ContainsKey((guestLineItem.ProductId, guestLineItem.VariantId)))
+					{
+						dictionary[(guestLineItem.ProductId, guestLineItem.VariantId)].Quantity += guestLineItem.Quantity;
+					}
+					else
+					{
+						dictionary.Add((guestLineItem.ProductId, guestLineItem.VariantId), guestLineItem);
+					}
+				}
+			}
+
+			if (loggedLineItems != null)
+			{
+				foreach (var loggedLineItem in loggedLineItems)
+				{
+					if (dictionary.ContainsKey((loggedLineItem.ProductId, loggedLineItem.VariantId)))
+					{
+						dictionary[(loggedLineItem.ProductId, loggedLineItem.VariantId)].Quantity += loggedLineItem.Quantity;
+					}
+					else
+					{
+						dictionary.Add((loggedLineItem.ProductId, loggedLineItem.VariantId), loggedLineItem);
+					}
+				}
+			}
+
+			return dictionary.Values.ToList();
+		}
+	}
 }
