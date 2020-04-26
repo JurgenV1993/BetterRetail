629befc4b178b009d61fad0e8a5bbc8f4008d935
vadym.hyryn <vadym.hyryn@orckestra.com>
2020-04-26 17:51:37 +0300

    Update ContextHelper.cs

--- src/Orckestra.Composer.CompositeC1/Context/ContextHelper.cs
+++ src/Orckestra.Composer.CompositeC1/Context/ContextHelper.cs
@@ -1,10 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web.Mvc;
 using Composite.Core.WebClient.Renderings.Page;
 
 namespace Orckestra.Composer.CompositeC1.Context
